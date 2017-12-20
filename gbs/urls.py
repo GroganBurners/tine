aa004d0b8b0c224577c969e2914008ada3f91547
Neil Grogan <neil@grogan.ie>
2017-12-20 16:02:29 +0000

    First excel export working

--- gbs/urls.py
+++ gbs/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('export/xls/', views.export_users_xls, name='export_users_xls'),
 ]
--- gbs/views.py
+++ gbs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from gbs.models import Carousel, Price
 from decimal import Decimal
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+import xlwt
 
 
 # Create your views here.
@@ -26,6 +29,37 @@ def index(request):
                 'oil_prices': oil_prices, 'summer_gas': summer_gas, 
                 'summer_oil': summer_oil, 'repair_fee': repair_fee }
     return render(request, 'index.html', context)
+
+
+def export_users_xls(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="users.xls"'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Users')
+
+    # Sheet header, first row
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = ['Username', 'First name', 'Last name', 'Email address', ]
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+
+    rows = User.objects.all().values_list('username', 'first_name', 'last_name', 'email')
+    for row in rows:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+    return response
 
 
 def get_season():
