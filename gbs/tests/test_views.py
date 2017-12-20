f9da1d824c2cc5700e6c00ae485b2faa26165224
Neil Grogan <neil@grogan.ie>
2017-12-20 15:43:23 +0000

    Added first tests

---
+++ gbs/tests/test_views.py
@@ -0,0 +1,19 @@
+import unittest
+from django.test import Client
+from gbs.models import Price
+
+
+class ViewsTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_homepage(self):
+        # Issue a GET request.
+        price = Price(type="repair_call_out", cost=80.00, summer_offer=False)
+        price.save()
+        response = self.client.get('/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(response.context['repair_fee']), "Repair Call Out Fee (First Hour) €80")
