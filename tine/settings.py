4506c6c739574678b000c2a7e8b576c79885bd88
Neil Grogan <neil@grogan.ie>
2017-12-21 12:47:20 +0000

    Added debug django env var

--- tine/settings.py
+++ tine/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", 'testsecretkeyfortesting')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['groganbuners.com','groganburners.ie','.serveo.net']
 
