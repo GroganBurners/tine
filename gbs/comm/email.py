4ac384d41efe973a0680f877192202b7c77ba150
Neil Grogan <neil@grogan.ie>
2018-01-14 19:44:16 +0000

    Email now can be html format

--- gbs/comm/email.py
+++ gbs/comm/email.py
@@ -8,13 +8,10 @@ from io import BytesIO
 from gbs.pdf import export_invoice
 
 def send_invoice(invoice):
-    pdf = BytesIO()
-    export_invoice(pdf, invoice)
-    pdf.seek(0)
-    attachment = MIMEApplication(pdf.read())
+    pdf = export_invoice(invoice)
+    attachment = MIMEApplication(pdf)
     attachment.add_header("Content-Disposition", "attachment",
                                           filename=invoice.file_name())
-    pdf.close()
 
     subject = f'Your Grogan Burner Services Invoice {invoice.invoice_id} is ready'
     email_kwargs = {
@@ -23,14 +20,12 @@ def send_invoice(invoice):
         "SUPPORT_EMAIL": app_settings.EMAIL,
     }
 
-    # try:
-    #     template = get_template("email/invoice.html")
-    #     body = template.render(Context(email_kwargs))
-    # except TemplateDoesNotExist:
-    body = render_to_string("email/invoice.txt", email_kwargs)
+    template = get_template("email/html/invoice-compiled.html")
+    html_body = template.render(email_kwargs)
+    body = render_to_string("email/txt/invoice.txt", email_kwargs)
 
     email = EmailMultiAlternatives(subject=subject, body=strip_tags(body), from_email='mick@grogan.ie', to=['neil@grogan.ie'])
-    # email.attach_alternative(body, "text/html")
+    email.attach_alternative(html_body, "text/html")
     email.attach(attachment)
     email.send(fail_silently=False)
 
