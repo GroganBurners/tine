84ceb293b987f3cf2a4a27da590c0025624861b5
Neil Grogan <neil@grogan.ie>
2018-01-14 23:33:12 +0000

    Added total row to Excel export, fixed col width

--- gbs/excel.py
+++ gbs/excel.py
@@ -1,6 +1,9 @@
 from .models import Invoice, Expense
 from openpyxl import Workbook
 from openpyxl.compat import range
+from openpyxl.styles import Font, Fill
+from openpyxl.styles.borders import Border, Side
+from openpyxl.worksheet.table import Table, TableStyleInfo
 from openpyxl.utils import get_column_letter
 from decimal import Decimal
 from io import BytesIO
@@ -10,15 +13,42 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def apply_border_format(ws, row, column, style='thin'):
+    thin_border = Border(left=Side(style=style), 
+                     right=Side(style=style), 
+                     top=Side(style=style), 
+                     bottom=Side(style=style))
+    ws.cell(row=row, column=column).border = thin_border
+
+def print_total_row(ws, row_num):
+    ws["A"+str(row_num)] = "Total"
+    for let in ['D','E','F','G','H','I']:
+        start_cell = f'{let}{row_num}'
+        ws[start_cell] = f"=SUM({let}2:{let}{row_num-1})"
+
+def adjust_column_widths(ws):
+    for col in ws.columns:
+     max_length = 0
+     column = col[0].column # Get the column name
+     for cell in col:
+         try: # Necessary to avoid error on empty cells
+             if len(str(cell.value)) > max_length:
+                 max_length = len(cell.value)
+         except:
+             pass
+     adjusted_width = (max_length + 2) * 1.2
+     ws.column_dimensions[column].width = adjusted_width
+
 def export_finances():
     wb = Workbook()
     ws = wb.active
     ws.title = "FinanceSheet2018"
 
     # Sheet header, first row
-    row_num = 0
-
+    row_num = 1
     ws.append(['Date', 'Details', 'Company', 'Money Out', 'Money In', 'VAT Due In', 'VAT Due Out', 'VAT Total', 'Total' ])
+    adjust_column_widths(ws)
+
     invoices = Invoice.objects.all()
     expenses = Expense.objects.all()
     logger.error(invoices)
@@ -33,10 +63,23 @@ def export_finances():
         if type(res) == Invoice:
             total = total + res.total()
             row = [res.date, items[0].description, 'Customer', '', res.total(), '', res.total_vat(), '', total]
+            ws.append(row)
+            row_num = row_num + 1
         else:
             total = total - res.total()
             row = [res.date, items[0].description, 'SUPPLIER', res.total(), '', res.total_vat(), '', '', total]
-        ws.append(row)
+            ws.append(row)
+            row_num = row_num + 1
+
+    row_num = row_num + 1
+    print_total_row(ws, row_num)
+
+    # tab = Table(displayName="Table1", ref="A1:I4")
+    # style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
+    #                         showLastColumn=False, showRowStripes=True, showColumnStripes=True)
+    # tab.tableStyleInfo = style
+    # ws.add_table(tab)
+    apply_border_format(ws,3,2)
 
     buffer = BytesIO()
     wb.save(buffer)
