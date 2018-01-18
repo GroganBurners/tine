34894c67478734fb6afc6d0bb08f22908a744bc7
Neil Grogan <neil@grogan.ie>
2018-01-18 13:26:23 +0000

    Updated DB settings

--- tine/settings.py
+++ tine/settings.py
@@ -95,11 +95,11 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT", 587)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME", ''),
-        'USER': os.environ.get("DB_USER", ''),
-        'PASSWORD': os.environ.get("DB_PASS", ''),
-        'HOST': os.environ.get("DB_HOST", ''),
-        'PORT': os.environ.get("DB_PORT", '')
+        'NAME': os.environ.get("POSTGRES_NAME", 'gbs'),
+        'USER': os.environ.get("POSTGRES_USER", 'postgres'),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", 'postgres'),
+        'HOST': os.environ.get("POSTGRES_HOST", 'localhost'),
+        'PORT': os.environ.get("POSTGRES_PORT", 5342)
     }
 }
 
