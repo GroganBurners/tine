c4b635e2a62d961318eaf61e2e206fa25d7ebd62
Neil Grogan <neil@grogan.ie>
2018-01-18 23:17:44 +0000

    Trying fix for selenium

--- tine/settings.py
+++ tine/settings.py
@@ -45,8 +45,8 @@ MEDIA_ROOT = '/usr/src/app/static'
 INSTALLED_APPS = [
     'gbs',
     'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.auth',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
