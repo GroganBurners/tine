2005095fba55d681b3103f71e1fb4d49885145be
Neil Grogan <neil@grogan.ie>
2018-01-18 13:53:55 +0000

    Updated travis config

--- tine/settings.py
+++ tine/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'USER': os.environ.get("POSTGRES_USER", 'postgres'),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD", 'postgres'),
         'HOST': os.environ.get("POSTGRES_HOST", 'localhost'),
-        'PORT': os.environ.get("POSTGRES_PORT", 5342)
+        'PORT': os.environ.get("POSTGRES_PORT", 5432)
     }
 }
 
