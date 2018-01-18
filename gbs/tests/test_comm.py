69eb1250bd6e73fd8bf8990edafc595aa2b3e52a
Neil Grogan <neil@grogan.ie>
2018-01-18 15:00:34 +0000

    Added tests for SMS

---
+++ gbs/tests/test_comm.py
@@ -0,0 +1,15 @@
+import unittest
+import responses
+from gbs.comm import sms
+
+
+class SMSTest(unittest.TestCase):
+    def setUp(self):
+        responses.add(responses.POST, sms.API_ENDPOINT,
+                      json={'success': True}, status=200)
+
+    @responses.activate
+    def test_sms_send(self):
+        response = sms.send_sms("Hello, World!", "+353890000000")
+        print(response)
+        self.assertEqual(response['success'], True)
