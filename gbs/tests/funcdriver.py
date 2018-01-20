9f8af17cc7f784b87a8c707f531abbbab5404228
Neil Grogan <neil@grogan.ie>
2018-01-20 14:51:54 +0000

    Add travis build data to SauceLabs tests

--- gbs/tests/funcdriver.py
+++ gbs/tests/funcdriver.py
@@ -5,11 +5,24 @@ from selenium import webdriver
 
 def get_driver():
         if os.getenv("SAUCE_ACCESS_KEY"):
+            # Set capabilities on sauce
             desired_cap = {
                 'platform': "Mac OS X 10.12",
                 'browserName': "safari",
                 'version': "11",
             }
+
+            # Add Travis metadata to the build
+            job = os.environ.get('TRAVIS_JOB_NUMBER')
+            if job is not None:
+                desired_cap['tunnel-identifier'] = job
+            build = os.environ.get("TRAVIS_BUILD_NUMBER")
+            if build is not None:
+                desired_cap['build'] = build
+            tag = os.environ.get("TRAVIS_PYTHON_VERSION")
+            if tag is not None:
+                desired_cap['tags'] = [tag, "CI"]
+
 
             user = os.environ.get('SAUCE_USERNAME')
             key = os.environ.get('SAUCE_ACCESS_KEY')
