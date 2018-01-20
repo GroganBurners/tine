44a056a3ed17c36229ad74fd1838b9667a7eaf41
Neil Grogan <neil@grogan.ie>
2018-01-20 16:06:23 +0000

    Travis try Selenium

--- gbs/tests/test_func.py
+++ gbs/tests/test_func.py
@@ -1,29 +1,108 @@
 import os
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.support.ui import WebDriverWait
-from unittest import skipUnless
-from gbs.tests.funcdriver import get_driver
+import sys
+
+from selenium import webdriver
+from django.test import LiveServerTestCase
+
+RUN_LOCAL = os.environ.get('RUN_TESTS_LOCAL') == 'True'
 
 
-@skipUnless(os.environ.get('DJANGO_SELENIUM_TESTS', False),
-            "Skipping Selenium tests")
-class FunctionalTest(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super(FunctionalTest, cls).setUpClass()
-        cls.selenium = get_driver()
+if RUN_LOCAL:
+    # could add Chrome, PhantomJS etc... here
+    browsers = ['Firefox']
+else:
+    from sauceclient import SauceClient
+    USERNAME = os.environ.get('SAUCE_USERNAME')
+    ACCESS_KEY = os.environ.get('SAUCE_ACCESS_KEY')
+    sauce = SauceClient(USERNAME, ACCESS_KEY)
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super(FunctionalTest, cls).tearDownClass()
+    browsers = [
+        {"platform": "Mac OS X 10.9",
+         "browserName": "chrome",
+         "version": "35"},
+        {"platform": "Windows 8.1",
+         "browserName": "internet explorer",
+         "version": "11"},
+        {"platform": "Linux",
+         "browserName": "firefox",
+         "version": "29"}]
 
-    def test_index(self):
-        print(self.live_server_url)
-        self.selenium.get(self.live_server_url)
-        self.selenium.get_screenshot_as_file('/tmp/website.png')
-        wait = WebDriverWait(self.selenium, 10)
-        print("TITLE:", self.selenium.title)
-        wait.until(
-            lambda selenium: self.selenium.title.lower().startswith('g'))
-        self.assertIn("Grogan Burner Services", self.selenium.title)
+
+def on_platforms(platforms, local):
+    if local:
+        def decorator(base_class):
+            module = sys.modules[base_class.__module__].__dict__
+            for i, platform in enumerate(platforms):
+                d = dict(base_class.__dict__)
+                d['browser'] = platform
+                name = "%s_%s" % (base_class.__name__, i + 1)
+                module[name] = type(name, (base_class,), d)
+            pass
+        return decorator
+
+    def decorator(base_class):
+        module = sys.modules[base_class.__module__].__dict__
+        for i, platform in enumerate(platforms):
+            d = dict(base_class.__dict__)
+            d['desired_capabilities'] = platform
+            name = "%s_%s" % (base_class.__name__, i + 1)
+            module[name] = type(name, (base_class,), d)
+    return decorator
+
+
+@on_platforms(browsers, RUN_LOCAL)
+class HelloSauceTest(LiveServerTestCase):
+    """
+    Runs a test using travis-ci and saucelabs
+    """
+
+    def setUp(self):
+        if RUN_LOCAL:
+            self.setUpLocal()
+        else:
+            self.setUpSauce()
+
+    def tearDown(self):
+        if RUN_LOCAL:
+            self.tearDownLocal()
+        else:
+            self.tearDownSauce()
+
+    def setUpSauce(self):
+        self.desired_capabilities['name'] = self.id()
+        self.desired_capabilities['tunnel-identifier'] = \
+            os.environ['TRAVIS_JOB_NUMBER']
+        self.desired_capabilities['build'] = os.environ['TRAVIS_BUILD_NUMBER']
+        self.desired_capabilities['tags'] = \
+            [os.environ['TRAVIS_PYTHON_VERSION'], 'CI']
+
+        print self.desired_capabilities
+
+        sauce_url = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
+        self.driver = webdriver.Remote(
+            desired_capabilities=self.desired_capabilities,
+            command_executor=sauce_url % (USERNAME, ACCESS_KEY)
+        )
+        self.driver.implicitly_wait(5)
+
+    def setUpLocal(self):
+        self.driver = getattr(webdriver, self.browser)()
+        self.driver.implicitly_wait(3)
+
+    def tearDownLocal(self):
+        self.driver.quit()
+
+    def tearDownSauce(self):
+        print("\nLink to your job: \n "
+              "https://saucelabs.com/jobs/%s \n" % self.driver.session_id)
+        try:
+            if sys.exc_info() == (None, None, None):
+                sauce.jobs.update_job(self.driver.session_id, passed=True)
+            else:
+                sauce.jobs.update_job(self.driver.session_id, passed=False)
+        finally:
+            self.driver.quit()
+
+    def test_sauce(self):
+        self.driver.get(self.live_server_url + '/admin')
+        assert "Log in" in self.driver.title
