f25a854cbc5bf5e15a21daebf5cffa0482472a66
Neil Grogan <neil.grogan@ericsson.com>
2018-01-22 13:54:35 +0000

    Added endpoint for new index

--- gbs/views.py
+++ gbs/views.py
@@ -27,3 +27,26 @@ def index(request):
                'oil_prices': oil_prices, 'summer_gas': summer_gas,
                'summer_oil': summer_oil, 'repair_fees': repair_fees}
     return render(request, 'index.html', context)
+
+def new_index(request):
+    gas_prices = Price.objects.filter(type__startswith="gas",
+                                      summer_offer=False,
+                                      cost__gt=Decimal('0.00')
+                                      ).exclude(type__endswith="install")
+    oil_prices = Price.objects.filter(type__startswith="oil",
+                                      summer_offer=False,
+                                      cost__gt=Decimal('0.00'))
+    summer_gas = Price.objects.filter(type__startswith="gas",
+                                      summer_offer=True,
+                                      cost__gt=Decimal('0.00'))
+    summer_oil = Price.objects.filter(type__startswith="oil",
+                                      summer_offer=True,
+                                      cost__gt=Decimal('0.00'))
+    repair_fees = Price.objects.filter(type__startswith="repair",
+                                      cost__gt=Decimal('0.00'))
+    # season = get_season()
+    carousels = Carousel.objects.filter(active=True).order_by('order')
+    context = {'carousels': carousels, 'gas_prices': gas_prices,
+               'oil_prices': oil_prices, 'summer_gas': summer_gas,
+               'summer_oil': summer_oil, 'repair_fees': repair_fees}
+    return render(request, 'new/index.html', context)
