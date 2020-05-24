3852e9500b4f3706ccf48546be9722e11945b2c4
Neil Grogan <neil@grogan.ie>
2020-05-24 16:41:30 +0100

    Updated terraform syntax

--- misc/terraform/lightsail.tf
+++ misc/terraform/lightsail.tf
@@ -18,12 +18,12 @@ resource "aws_lightsail_domain" "groganburners_com" {
 # Upload our SSH Public Key
 resource "aws_lightsail_key_pair" "personal_key_pair" {
   name       = "id_personal"
-  public_key = "${file("~/.ssh/id_personal.pub")}"
+  public_key = "file('~/.ssh/id_personal.pub')"
 }
 
 # Create a new Lightsail Instance
 resource "aws_lightsail_instance" "gbs_vps" {
-  name              = "Ubuntu18.04-Ireland"
+  name              = "Ubuntu18.04-1GB-Ireland1"
   availability_zone = "eu-west-1a"
   blueprint_id      = "ubuntu_18_04"
   bundle_id         = "micro_2_0"
@@ -32,6 +32,6 @@ resource "aws_lightsail_instance" "gbs_vps" {
 
 # Attach Static IP to our New Instance
 resource "aws_lightsail_static_ip_attachment" "test" {
-  static_ip_name = "${aws_lightsail_static_ip.static_ip_ire.id}"
-  instance_name  = "${aws_lightsail_instance.gbs_vps.id}"
+  static_ip_name = "aws_lightsail_static_ip.static_ip_ire.id"
+  instance_name  = "aws_lightsail_instance.gbs_vps.id"
 }
